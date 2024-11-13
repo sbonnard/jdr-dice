0fc272668077e08cf969ab15b3d9ab5f0b2572d4
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-11-13 22:59:18 +0100

    added every dice on page

--- app/index.php
+++ app/index.php
@@ -19,12 +19,46 @@
     </header>
 
     <main class="container">
-        <h2>Dé 100</h2>
-        <img src="img/dice-white.svg" alt="Dé de JDR blanc">
+
+        <section class="dice__section" aria-labelledby="ttl100">
+            <h2 class="ttl dice__word" id="ttl100">Dé <span class="number">100</span></h2>
+            <button><img src="img/100.png" alt="Dé 100 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl20">
+            <h2 class="ttl dice__word" id="ttl20">Dé <span class="number">20</span></h2>
+            <button><img src="img/20.png" alt="Dé 20 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl12">
+            <h2 class="ttl dice__word" id="ttl12">Dé <span class="number">12</span></h2>
+            <button><img src="img/12.png" alt="Dé 12 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl10">
+            <h2 class="ttl dice__word" id="ttl10">Dé <span class="number">10</span></h2>
+            <button><img src="img/10.png" alt="Dé 10 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl8">
+            <h2 class="ttl dice__word" id="ttl8">Dé <span class="number">8</span></h2>
+            <button><img src="img/8.png" alt="Dé 8 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl6">
+            <h2 class="ttl dice__word" id="ttl6">Dé <span class="number">6</span></h2>
+            <button><img src="img/6.png" alt="Dé 6 de JDR"></button>
+        </section>
+
+        <section class="dice__section" aria-labelledby="ttl4">
+            <h2 class="ttl dice__word" id="ttl4">Dé <span class="number">4</span></h2>
+            <button><img src="img/4.png" alt="Dé 4 de JDR"></button>
+        </section>
+
     </main>
 
     <footer class="footer">
-
+        <p>Pas par là !</p>
     </footer>
 
 </body>
