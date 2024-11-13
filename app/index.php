ac2c58c219be1c771a678c738d092b4f82cd4ecc
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-11-13 23:14:15 +0100

    Did manby stuff on style and added buttons. Worked well on banners results too

--- app/index.php
+++ app/index.php
@@ -15,44 +15,51 @@
 <body>
 
     <header class="header bg-blur">
-        <h1 class="ttl ttl--main">JDR Fratrie</h1>
+        <h1 class="ttl ttl--main">Don't Roll Single</h1>
     </header>
 
     <main class="container">
 
         <section class="dice__section" aria-labelledby="ttl100">
+            <div class="dice__banner" id="banner100">100</div>
             <h2 class="ttl dice__word" id="ttl100">Dé <span class="number">100</span></h2>
-            <button><img src="img/100.png" alt="Dé 100 de JDR"></button>
+            <button id="dice100"><img src="img/100.png" alt="Dé 100 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl20">
+            <div class="dice__banner" id="banner20">20</div>
             <h2 class="ttl dice__word" id="ttl20">Dé <span class="number">20</span></h2>
-            <button><img src="img/20.png" alt="Dé 20 de JDR"></button>
+            <button id="dice20"><img src="img/20.png" alt="Dé 20 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl12">
+            <div class="dice__banner" id="banner12">12</div>
             <h2 class="ttl dice__word" id="ttl12">Dé <span class="number">12</span></h2>
-            <button><img src="img/12.png" alt="Dé 12 de JDR"></button>
+            <button id="dice12"><img src="img/12.png" alt="Dé 12 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl10">
+            <div class="dice__banner" id="banner10">10</div>
             <h2 class="ttl dice__word" id="ttl10">Dé <span class="number">10</span></h2>
-            <button><img src="img/10.png" alt="Dé 10 de JDR"></button>
+            <button id="dice10"><img src="img/10.png" alt="Dé 10 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl8">
+            <div class="dice__banner" id="banner8">8</div>
             <h2 class="ttl dice__word" id="ttl8">Dé <span class="number">8</span></h2>
-            <button><img src="img/8.png" alt="Dé 8 de JDR"></button>
+            <button id="dice8"><img src="img/8.png" alt="Dé 8 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl6">
+            <div class="dice__banner" id="banner6">6</div>
             <h2 class="ttl dice__word" id="ttl6">Dé <span class="number">6</span></h2>
-            <button><img src="img/6.png" alt="Dé 6 de JDR"></button>
+            <button id="dice6"><img src="img/6.png" alt="Dé 6 de JDR"></button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl4">
+            <div class="dice__banner" id="banner4">4</div>
             <h2 class="ttl dice__word" id="ttl4">Dé <span class="number">4</span></h2>
-            <button><img src="img/4.png" alt="Dé 4 de JDR"></button>
+            <button id="dice4"><img src="img/4.png" alt="Dé 4 de JDR"></button>
         </section>
 
     </main>
