a132f716fd2df68dd9227e876d57468492a7cd1c
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-11-13 14:47:06 +0100

    scss bugged ?

--- app/index.php
+++ app/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,21 @@
     <link rel="stylesheet" href="scss/style.scss">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
+
 <body>
-    
+
+    <header>
+        <h1 class="ttl ttl--main">JDR Fratrie</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
 </body>
+
 </html>
