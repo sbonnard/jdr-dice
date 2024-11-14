99106901df664cc901930404f7186fe350c95ec2
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-11-14 12:55:08 +0100

    get random draws when click and crit managed. Need to fix xsome stuff but pretty cool

--- app/index.php
+++ app/index.php
@@ -21,57 +21,57 @@
     <main class="container">
 
         <section class="dice__section" aria-labelledby="ttl100">
-            <div class="dice__banner" id="banner100">100</div>
+            <div class="dice__banner" data-banner="100" id="banner100">100</div>
             <h2 class="ttl dice__word" id="ttl100">Dé <span class="number">100</span></h2>
-            <button class="dice" id="dice100">
-                <img src="img/100.png" alt="Dé 100 de JDR">
+            <button class="dice" id="dice100" data-dice="100">
+                <img class="" src="img/100.png" alt="Dé 100 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl20">
-            <div class="dice__banner" id="banner20">20</div>
+            <div class="dice__banner" data-banner="20" id="banner20">20</div>
             <h2 class="ttl dice__word" id="ttl20">Dé <span class="number">20</span></h2>
-            <button id="dice20">
+            <button class="dice" id="dice20" data-dice="20">
                 <img src="img/20.png" alt="Dé 20 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl12">
-            <div class="dice__banner" id="banner12">12</div>
+            <div class="dice__banner" data-banner="12" id="banner12">12</div>
             <h2 class="ttl dice__word" id="ttl12">Dé <span class="number">12</span></h2>
-            <button id="dice12">
+            <button class="dice" id="dice12" data-dice="12">
                 <img src="img/12.png" alt="Dé 12 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl10">
-            <div class="dice__banner" id="banner10">10</div>
+            <div class="dice__banner" data-banner="10" id="banner10">10</div>
             <h2 class="ttl dice__word" id="ttl10">Dé <span class="number">10</span></h2>
-            <button id="dice10">
+            <button class="dice" id="dice10" data-dice="10">
                 <img src="img/10.png" alt="Dé 10 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl8">
-            <div class="dice__banner" id="banner8">8</div>
+            <div class="dice__banner" data-banner="8" id="banner8">8</div>
             <h2 class="ttl dice__word" id="ttl8">Dé <span class="number">8</span></h2>
-            <button id="dice8">
+            <button class="dice" id="dice8" data-dice="8">
                 <img src="img/8.png" alt="Dé 8 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl6">
-            <div class="dice__banner" id="banner6">6</div>
+            <div class="dice__banner" data-banner="6" id="banner6">6</div>
             <h2 class="ttl dice__word" id="ttl6">Dé <span class="number">6</span></h2>
-            <button id="dice6">
+            <button class="dice" id="dice6" data-dice="6">
                 <img src="img/6.png" alt="Dé 6 de JDR">
             </button>
         </section>
 
         <section class="dice__section" aria-labelledby="ttl4">
-            <div class="dice__banner" id="banner4">4</div>
+            <div class="dice__banner" data-banner="4" id="banner4">4</div>
             <h2 class="ttl dice__word" id="ttl4">Dé <span class="number">4</span></h2>
-            <button id="dice4">
+            <button class="dice" id="dice4" data-dice="4">
                 <img src="img/4.png" alt="Dé 4 de JDR">
             </button>
         </section>
@@ -83,5 +83,7 @@
     </footer>
 
 </body>
+
+<script type="module" src="js/dices.js"></script>
 
 </html>
