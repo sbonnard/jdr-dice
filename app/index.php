6704cbdee7936c0066bf8bee68a59f6a68e60d79
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-12-02 16:32:58 +0100

    updated readme and title

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JDR Fratrie</title>
+    <title>Don't Roll Single</title>
     <link rel="stylesheet" href="scss/style.scss">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <!-- if development -->
